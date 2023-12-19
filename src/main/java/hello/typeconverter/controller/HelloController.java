a82e5417e79bf4cf74ddb06110b0f83dbd50e186
iyeonghun <twosoull@naver.com>
2023-12-19 22:42:55 +0900

    converter 뷰에서 사용하기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +22,18 @@ public class HelloController {
 
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data){
-
         System.out.println("data= " + data);
-
         return "ok";
 
     }
 
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("ipPort ip = " + ipPort.getIp());
+        System.out.println("ipPort port = " + ipPort.getPort());
+
+        return "ok";
+    }
 }
 
 
